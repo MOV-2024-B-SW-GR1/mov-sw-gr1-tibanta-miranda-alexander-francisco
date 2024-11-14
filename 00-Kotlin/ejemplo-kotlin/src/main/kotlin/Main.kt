582c92658daae4c68b9c46309cfb72fae7e7f228
Alexander Tibanta Miranda <119013329+Alexander-Tibanta23@users.noreply.github.com>
2024-11-14 06:59:13 -0500

    Add unversioned files

---
+++ 00-Kotlin/ejemplo-kotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+package ec.edu.epn
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// Kotlin Hello World Program
+fun main(args: Array<String>) {
+    println("Hello, World!")
+}
