515ea01cf09ec6099c89b8ed51ce2278b5d35876
Alexander Tibanta Miranda <119013329+Alexander-Tibanta23@users.noreply.github.com>
2024-11-27 12:53:51 -0500

    Practica calificadores de recursos 21 de noviembre del 2024

--- 01-Android/app/src/main/java/com/example/sw2024bgr1_aftm/MainActivity.kt
+++ 01-Android/app/src/main/java/com/example/sw2024bgr1_aftm/MainActivity.kt
@@ -1,15 +1,17 @@
 package com.example.sw2024bgr1_aftm
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.setPadding
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : Activity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
